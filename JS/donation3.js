25e99956ca2e589f720cde7d1ad18fcc508466fe
Sreedhar-roy <sreedharroy0@gmail.com>
2024-09-24 22:39:05 +0600

    button bag fixed commit

--- JS/donation3.js
+++ JS/donation3.js
@@ -21,9 +21,10 @@ document.getElementById('btn-donate3').addEventListener('click', function(event)
     }
  
     const div = document.createElement('div');
-    div.classList.add('bg-red-100');
+    div.classList.add('bg-gray-100');
     div.innerHTML = `
-    <h4>${NewDontaion} ${title}</h4>`
+    <h4 class="text-xl text-gray-500">${NewDontaion} ${title}</h4>
+    <p class="text-xs text-gray-500">${new Date().toLocaleString()}</p>`
 
     document.getElementById('history-report').appendChild(div);
 })
--- JS/features.js
+++ JS/features.js
@@ -1,12 +1,33 @@
 
-document.getElementById('show-donation-section').addEventListener('click', function () {
-    const bg = document.getElementById('show-donation-section');
-    bg.style.backgroundColor= '#B4F461';
-    ShowSectionById('add-Donation-section');    
-})
+ document.getElementById('show-donation-section').addEventListener('click', function () {
+     ShowSectionById('add-Donation-section');    
+ })
 
-document.getElementById('show-history-section').addEventListener('click', function () {
-    const bg = document.getElementById('show-history-section');
-    bg.style.backgroundColor = '#B4F461';
-    ShowSectionById('add-history-section');
+ document.getElementById('show-history-section').addEventListener('click', function () {
+     ShowSectionById('add-history-section');
+ })
+
+
+const donation = document.getElementById('show-donation-section');
+const history = document.getElementById('show-history-section');
+
+history.addEventListener('click', function () {
+    history.classList.add(
+        "bg-[#b4f461]",        
+    );
+    history.classList.remove("text-gray-600");
+    donation.classList.remove(
+        "bg-[#b4f461]",        
+    );
+    donation.classList.add("text-gray-600")
+});
+donation.addEventListener('click', function(){
+    donation.classList.add(
+        "bg-[#b4f461]",        
+    );
+    donation.classList.remove("text-gray-600");
+    history.classList.remove(
+        "bg-[#b4f461]",        
+    );
+    history.classList.add("text-gray-600")
 })
