cf5bb7d43051a747842f614e98a09e35276d26ee
Sreedhar-roy <sreedharroy0@gmail.com>
2024-09-24 23:37:11 +0600

    project done

--- JS/donation2.js
+++ JS/donation2.js
@@ -18,8 +18,8 @@ document.getElementById('btn-donate2').addEventListener('click', function(event)
     const div = document.createElement('div');
     div.classList.add('bg-gray-100');
     div.innerHTML = `
-    <h4 class="text-xl text-gray-500">${NewDontaion} ${title}</h4>
-    <p class="text-xs text-gray-500">${new Date().toLocaleString()}</p>`
+   <h4 class="text-xl p-6 border text-gray-500">${NewDontaion} ${title}</h4>
+    <p class="text-xs p-2 border mb-4 text-gray-500">${new Date().toLocaleString()}</p>`
 
     document.getElementById('history-report').appendChild(div);
 })
--- JS/donation3.js
+++ JS/donation3.js
@@ -1,13 +1,13 @@
-document.getElementById('btn-donate3').addEventListener('click', function(event){
+document.getElementById('btn-donate3').addEventListener('click', function (event) {
     event.preventDefault();
     const NewDontaion = getDonationById('input-donate3');
     const CurrentTextDonation = getDonationTextId('Current-Donate3');
 
-    if(NewDontaion <= 0 || isNaN(NewDontaion)){
+    if (NewDontaion <= 0 || isNaN(NewDontaion)) {
         alert('Failed to your donation');
-        return ; 
+        return;
     }
-    else{
+    else {
         alert('CONGRATULATION SUCCESS YOUR DONATION');
     }
 
@@ -19,8 +19,8 @@ document.getElementById('btn-donate3').addEventListener('click', function(event)
     const div = document.createElement('div');
     div.classList.add('bg-gray-100');
     div.innerHTML = `
-    <h4 class="text-xl text-gray-500">${NewDontaion} ${title}</h4>
-    <p class="text-xs text-gray-500">${new Date().toLocaleString()}</p>`
+  <h4 class="text-xl p-6 border text-gray-500">${NewDontaion} ${title}</h4>
+    <p class="text-xs p-2 border mb-4 text-gray-500">${new Date().toLocaleString()}</p>`
 
     document.getElementById('history-report').appendChild(div);
 })
